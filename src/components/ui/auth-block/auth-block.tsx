69d350c6be6cfa8014c711bbfcaef4d5a41a6548
Таня Глубокая <t.glubokaia@gmail.com>
2023-08-31 16:37:47 +0300

    feature: Добавляет стили блоку для авторизации

--- src/components/ui/auth-block/auth-block.tsx
+++ src/components/ui/auth-block/auth-block.tsx
@@ -1,21 +1,32 @@
+import styles from './styles.module.css';
+
 export default function AuthBlock() {
+  const isSignedIn = false;
+
+  let AuthText;
+  let AuthLink;
+
+  if (isSignedIn) {
+    AuthText = (
+      <div className={styles.infoAuth}>
+        <p className={styles.infoText}>Проверьте, что можете получать принимать сообщения от <a href="#">чат-бота группы</a> для получения уведомлений</p>
+        <a className={styles.checkLink} href="#">Проверить</a>
+      </div>);
+    AuthLink = <a className={styles.orgAuthLink} href="#">Выход</a>;
+  } else {
+    AuthText = (
+      <div className={styles.infoNoAuth}>
+        <p className={styles.infoText}>Чтобы зарегистрироваться на мероприятия необходимо авторизоваться через ВКонтакте</p>
+        <a className={styles.visitorAuthLink} href="#">Войти через ВК</a>
+      </div>
+    );
+    AuthLink = <a className={styles.orgAuthLink} href="#">Вход для админов</a>;
+  }
 
   return (
-    <>
-      <article>
-        <div>
-          <p>Чтобы зарегистрироваться на мероприятия необходимо авторизоваться через ВКонтакте</p>
-          <a href="#">Войти через ВК</a>
-        </div>
-        <a href="#">Вход для админов</a>
-      </article>
-      <article>
-        <div>
-          <p>Чтобы зарегистрироваться на мероприятия необходимо авторизоваться через ВКонтакте</p>
-          <a href="#">Войти через ВК</a>
-        </div>
-      </article>
-    </>
-
+    <article>
+      {AuthText}
+      {AuthLink}
+    </article>
   )
-} 
+}
