76cadf09f5a49fe5db0b9d652ed9511d38028c45
Таня Глубокая <t.glubokaia@gmail.com>
2023-08-31 16:00:02 +0300

    feat: add auth block component for not authorized status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
+import AuthBlock from "@/components/auth-block/auth-block";
 import { ROUTES } from "@/core/routes";
 
 export default function Home() {
   return (
     <main>
       <Link href={ROUTES.application.users.path}>UsersScreen</Link>
+      <AuthBlock />
     </main>
   );
 }
